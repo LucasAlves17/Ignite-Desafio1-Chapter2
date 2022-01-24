8c99557d751e375fcbd7417e0e5d19f24d3402f8
LucasAlves17 <lukasmartinsalves17@gmail.com>
2022-01-24 06:09:20 -0300

    progresso no listar usuarios

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,10 +6,11 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
+    const user_id = request.headers.user_id as string;
 
     const users = this.listAllUsersUseCase.execute({ user_id });
-    return response.status(200).json();
+
+    return response.status(200).json(users);
   }
 }
 
